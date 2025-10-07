da836477fee7ae880884d075b62bca4d55fa3ac6
unknown <datnguyentien0803@gmail.com>
2025-10-07 08:48:51 +0700

    create

---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,13 @@
+const Header = () => {
+  return (
+    <div className="w-full h-24 ">
+      <HeaderTop />
+    </div>
+  );
+};
+
+const HeaderTop = () => {
+  return <div className="w-full h-16 bg-[#2a83e9] "></div>;
+};
+
+export default Header;
